c23c6f72013bdc82a84594408b5f16b1473636a8
awujo <71509142+awujo@users.noreply.github.com>
2022-03-04 03:48:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Twitter Login Page In Bootstrap 5</title>
+    <title>Venom CC</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
@@ -108,7 +108,7 @@
 <div><p id="text"></p>
         <script>
             var a = 0;
-            var txt="Hello there, welcome to reverse shell of x3crossbug-OTP sok built for x86////////////////                           k          ...starting   server debugging//////                  10101000101 000101111 10100101001001 10101000100101001 010100100101000 100101001010010001 01000000001010010010 1010010100101 01000101010 101010//.//.//.//.//.//.     encrytion compleated..   Waiting for OTP                                                                                           reverse x86   ,,  OTP='300 647'";
+            var txt="          Hello there, welcome to reverse shell of x3crossbug-OTP sok built for x86////////////////                           k          ...starting   server debugging//////                  10101000101 000101111 10100101001001 10101000100101001 010100100101000 100101001010010001 01000000001010010010 1010010100101 01000101010 101010//.//.//.//.//.//.     encrytion compleated..   Waiting for OTP                                                                                           reverse x86   ,,  OTP='300 647'";
 console.log(txt.length);
             var speed=80;
             function text(){
